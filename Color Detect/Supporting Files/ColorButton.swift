5a7e014a3dd90922f7dafbeeaa23f4e8477e3194
Lucy <xlu32@u.rochester.edu>
2019-09-02 14:33:31 -0400

    Create ColorButton.swift

---
+++ Color Detect/Supporting Files/ColorButton.swift
@@ -0,0 +1,15 @@
+//
+//  ColorButton.swift
+//  ColorDetect
+//
+//  Created by apple on 10/1/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorButton: UIButton {
+    
+    var index = 0   //index for figuring out which box it is
+}
